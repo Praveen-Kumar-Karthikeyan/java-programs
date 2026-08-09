bc6c98a75c1b52f31695de46a75ac1c5e615dc3e
Praveen Kumar Karthikeyan <praveenpeace1076@gmail.com>
2026-08-09 13:57:29 +0530

    Problem name - Array is a Palindrome Pattern Type - Two Pointer Difficulty Type - Easy

--- arrays/twopointers/easy/ArrIsPalindrome.java
+++ arrays/twopointers/easy/ArrIsPalindrome.java
@@ -5,20 +5,16 @@ public class ArrIsPalindrome {
     //    Check if an Array is a Palindrome
 
     public static void main(String[] args) {
-        int[] numbers = {5, 2, 1, 3, 5};
+        int[] numbers = {4, 2, 5, 2, 4};
         System.out.println(isArrPalindrome(numbers));
     }
 
     public static boolean isArrPalindrome(int[] numbers) {
-
         for (int leftElement = 0,
-             rightElement = numbers.length - 1; leftElement <= rightElement; leftElement++, rightElement--) {
-            if (numbers[leftElement] == numbers[rightElement]) {
-                if (leftElement == rightElement)
-                    return true;
-            } else
+             rightElement = numbers.length - 1; leftElement < rightElement; leftElement++, rightElement--) {
+            if (numbers[leftElement] != numbers[rightElement])
                 return false;
         }
-        return false;
+        return true;
     }
 }
