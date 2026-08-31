a7423bc801cbb682fa136dd9420927ee7402ac10
Praveen Kumar Karthikeyan <praveenpeace1076@gmail.com>
2026-08-31 23:06:38 +0530

    Problem name - Find frequency of characters in a string Pattern Type - Using Map Difficulty Type - Easy

---
+++ Strings/easy/FrequencyOfCharacters.java
@@ -0,0 +1,20 @@
+package Strings.easy;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FrequencyOfCharacters {
+
+    public static void main(String[] args) {
+        String msg = "HelloWorld";
+        System.out.println(frequencyOfCharacters(msg));
+    }
+
+    public static Map<Character, Integer> frequencyOfCharacters(String msg) {
+        Map<Character, Integer> freq = new LinkedHashMap<>();
+        for (char element : msg.toCharArray()) {
+            freq.put(element, freq.getOrDefault(element, 0) + 1);
+        }
+        return freq;
+    }
+}
