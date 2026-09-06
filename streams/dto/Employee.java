963961e0e1ecc57a94af81407963f122481feb9d
Praveen Kumar Karthikeyan <praveenpeace1076@gmail.com>
2026-09-06 20:12:29 +0530

    Problem name - Employee Object related Changes Pattern Type - Using Streams Difficulty Type - Easy

--- streams/dto/Employee.java
+++ streams/dto/Employee.java
@@ -7,16 +7,28 @@ public class Employee {
     private String empName;
     private Integer empId;
     private Long salary;
+    private String department;
+    private Integer age;
     private LocalDate joiningDate;
 
     public Employee() {
     }
 
-    public Employee(String empName, Integer empId, Long salary, LocalDate joiningDate) {
+    public Employee(String empName, Integer empId, Long salary, String department, Integer age, LocalDate joiningDate) {
         this.empName = empName;
         this.empId = empId;
         this.salary = salary;
+        this.department = department;
+        this.age = age;
         this.joiningDate = joiningDate;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public Integer getAge() {
+        return age;
     }
 
     public String getEmpName() {
