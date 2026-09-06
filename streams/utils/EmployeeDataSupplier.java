d50ebbe6d2fff4e792ad7778e9a24dcf7dedc579
Praveen Kumar Karthikeyan <praveenpeace1076@gmail.com>
2026-09-06 22:34:52 +0530

    Problem name - Employee Object related stream programs revision Pattern Type - Using Streams Difficulty Type - Easy

--- streams/utils/EmployeeDataSupplier.java
+++ streams/utils/EmployeeDataSupplier.java
@@ -3,12 +3,13 @@ package streams.utils;
 import streams.dto.Employee;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class EmployeeDataSupplier {
 
-    private EmployeeDataSupplier() {}
+    private EmployeeDataSupplier() {
+    }
 
     public static List<Employee> getEmployees() {
         Employee employeeOne = new Employee("Jimmy", 101, 98000L, "Developer", 29, LocalDate.of(1998, 9, 8));
@@ -21,6 +22,54 @@ public class EmployeeDataSupplier {
         listOfEmployees.add(employeeThree);
         listOfEmployees.add(employeeFour);
         return listOfEmployees;
+    }
+
+    public static void main(String[] args) {
+        List<Employee> employeeList = getEmployees();
+        //Find employees whose salary > No
+        List<Employee> employeesWhoseSalGreaterThan68K =
+                employeeList.stream().filter(emp -> emp.getSalary() > 68000).toList();
+        System.out.println(employeesWhoseSalGreaterThan68K);
+        // Get employee names
+        List<String> empNames = employeeList.stream().map(Employee::getEmpName).toList();
+        System.out.println(empNames);
+        // Find highest-paid employee
+        Optional<Employee> highestPaidEmp =
+                employeeList.stream().max(Comparator.comparingLong(Employee::getSalary));
+        System.out.println(highestPaidEmp.orElse(null));
+        // min Paid employee
+        Optional<Employee> minimumPaidEmployee = employeeList.stream().min(Comparator.comparingLong(Employee::getSalary));
+        System.out.println(minimumPaidEmployee);
+        // Find average salary
+        OptionalDouble avgSal = employeeList.stream().mapToLong(Employee::getSalary).average();
+        System.out.println("Average salary " + avgSal);
+        // Group Emp by Dep
+        Map<String, List<Employee>> employeeByDep =
+                employeeList.stream().collect(Collectors.groupingBy(Employee::getDepartment));
+        System.out.println("employeeByDep " + employeeByDep);
+
+        Map<String, Long> empCountInEachDep =
+                employeeList.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.counting()));
+        System.out.println("empCountInEachDep " + empCountInEachDep);
+
+        Map<String, Double> highestSalEmpFromEachDept =
+                employeeList.stream().collect(Collectors.groupingBy(
+                        Employee::getDepartment,
+                        Collectors.averagingLong(Employee::getSalary)
+                ));
+        System.out.println("highestSalEmpFromEachDept " + highestSalEmpFromEachDept);
+
+
+        Optional<Map.Entry<String, Double>> highestAvgSalByDep = employeeList.stream().collect(
+                        Collectors.groupingBy(Employee::getDepartment,
+                                Collectors.averagingLong(Employee::getSalary)))
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue());
+        System.out.println(highestAvgSalByDep.orElse(null));
+
+
+
     }
 
 }
