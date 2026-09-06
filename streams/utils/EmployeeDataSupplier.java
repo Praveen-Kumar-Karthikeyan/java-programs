524e74325a49b1d0f7ea509484e36e132a5a12c4
Praveen Kumar Karthikeyan <praveenpeace1076@gmail.com>
2026-09-06 20:08:26 +0530

    Problem name - Easy streams related programs Pattern Type - Using Streams Difficulty Type - Easy

--- streams/utils/EmployeeDataSupplier.java
+++ streams/utils/EmployeeDataSupplier.java
@@ -8,14 +8,13 @@ import java.util.List;
 
 public class EmployeeDataSupplier {
 
-    private EmployeeDataSupplier() {
-    }
+    private EmployeeDataSupplier() {}
 
     public static List<Employee> getEmployees() {
-        Employee employeeOne = new Employee("Jimmy", 101, 98000L, LocalDate.of(1998, 9, 8));
-        Employee employeeTwo = new Employee("Mike", 102, 68000L, LocalDate.of(1998, 12, 23));
-        Employee employeeThree = new Employee("Kim Wexler", 103, 88000L, LocalDate.of(1999, 10, 2));
-        Employee employeeFour = new Employee("Gustavo", 104, 987651L, LocalDate.of(1996, 1, 1));
+        Employee employeeOne = new Employee("Jimmy", 101, 98000L, "Developer", 29, LocalDate.of(1998, 9, 8));
+        Employee employeeTwo = new Employee("Mike", 102, 68000L, "Testing", 25, LocalDate.of(1998, 12, 23));
+        Employee employeeThree = new Employee("Kim Wexler", 103, 88000L, "Developer", 34, LocalDate.of(1999, 10, 2));
+        Employee employeeFour = new Employee("Gustavo", 104, 987651L, "Testing", 39, LocalDate.of(1996, 1, 1));
         List<Employee> listOfEmployees = new ArrayList<>();
         listOfEmployees.add(employeeOne);
         listOfEmployees.add(employeeTwo);
